83c873e16791283131e0869d88dcbc898a67fdeb
martincostello <martin@martincostello.com>
2023-02-03 13:57:00 +0000

    Fix code analysis warnings
    - Pass exception through to `LogError()`.
    - Add calls to `ConfigureAwait(false).
    - Add missing XML documentation.
    - Suppress CA1032 on `SSMParameterToLongException` (sic).
    - Suppress CA1031.
    - Remove redundant assignment and namespace.

--- src/Amazon.AspNetCore.DataProtection.SSM/SSMParameterToLongException.cs
+++ src/Amazon.AspNetCore.DataProtection.SSM/SSMParameterToLongException.cs
@@ -6,8 +6,14 @@ namespace Amazon.AspNetCore.DataProtection.SSM
     /// Thrown when a parameter should be stored that exceeds the configured <see cref="PersistOptions.TierStorageMode"/>
     /// tiers maximum length.
     /// </summary>
+#pragma warning disable CA1032 // Implement standard exception constructors
     public class SSMParameterToLongException : Exception
+#pragma warning restore CA1032 // Implement standard exception constructors
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SSMParameterToLongException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public SSMParameterToLongException(string message) : base(message)
         {
         }
